a1ae6752700568a4cd007a28cf61c20497ea4b92
peakfind <peakfind@126.com>
2024-04-19 17:14:47 +0800

    the first commit

---
+++ cim.jl
@@ -0,0 +1,11 @@
+#=---------------------------------------------------------
+cim.jl
+
+
+---------------------------------------------------------=#
+
+function beyn(D, L)
+    # ComplexF64 are aliases for Complex{Float64}
+    A0 = zeros(ComplexF64, D, L)
+    A1 = zeros(ComplexF64, D, L)
+end
