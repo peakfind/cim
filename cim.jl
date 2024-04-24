1ef8a49a4a0a315b17cd8d69771000c4a3a882a5
peakfind <peakfind@126.com>
2024-04-24 17:02:01 +0800

    compute the matrix B

--- cim.jl
+++ cim.jl
@@ -1,6 +1,6 @@
 #=---------------------------------------------------------
 - cim.jl
--
+- TODO: we should write a script to implement the method first
 -
 ---------------------------------------------------------=#
 
@@ -9,6 +9,7 @@ using LinearAlgebra
 # contours that we plan to support
 abstract type contour end
 
+# TODO: Can we provide a generic method for different types of contours?
 struct ellipse <: contour
     center::Vector{Float64}
     semi_x::Float64
@@ -26,14 +27,21 @@ struct circle <: contour
     radius::Float64
 end
 
+# TODO: how to combine quadrature rules with the contours?
+struct quadrature{shape}
+    reference::shape
+    nodes::Vector{Vector{Float64}}
+    weights::Vector{Float64}
+end
 
+#  N the number of quadrature nodes
 function cont_int(ellip::ellipse, D, L, N)
 
     A0 = zeros(ComplexF64, D, L)
     A1 = zeros(ComplexF64, D, L)
 
     # generate random matrix Vhat
-    Vhat = randn(ComplexF64, D, L)
+    V̂ = randn(ComplexF64, D, L)
 
     # compute A0 and A1 (contour integrals with some quadrature rules)
     DeltaTheta = 2.0*pi/N
@@ -45,15 +53,25 @@ function cont_int(ellip::ellipse, D, L, N)
         z1 = ellip.semi_x*c + ellip.semi_y*s
     end
 
-
-
-
     # SVD of A0
+    (V, Σ, W) = svd(A0)
+
+    tol = 1.0e-8
+    k = length(findall(abs(Σ) .> tol))
+    println("Find $(k) nonzero singular values.")
 
     # perform a rank test
 
     # compute the matrix B
+    Vk = V[:, 1:k]
+    Simgak = Σ[1:k]
+    Wk = W[:, 1:k]
+    B = Vk' * A1 * Wk * diagm(1 ./ Sigmak)
 
     # solve the eigenvalue problem for B
+    λ = eigvals(B)
+    V = eigvecs(B)
+
+    return λ, V
     
 end
