dee86bef17b1044e7e74ac0f21ac6098a53277dc
peakfind <peakfind@126.com>
2024-04-25 20:09:00 +0800

    integral A0

--- cim.jl
+++ cim.jl
@@ -34,44 +34,50 @@ struct quadrature{shape}
     weights::Vector{Float64}
 end
 
-#  N the number of quadrature nodes
-function cont_int(ellip::ellipse, D, L, N)
+#  N     : the number of quadrature nodes
+#  ellip : the contour
+#  D     :
+#  L     :
+#  T     : the matrix of the NEP
+function cont_int(ellip::ellipse, T, D, L, N)
 
     A0 = zeros(ComplexF64, D, L)
     A1 = zeros(ComplexF64, D, L)
 
     # generate random matrix Vhat
-    V̂ = randn(ComplexF64, D, L)
+    Vhat = randn(ComplexF64, D, L)
 
     # compute A0 and A1 (contour integrals with some quadrature rules)
-    DeltaTheta = 2.0*pi/N
+	# here we use trapezoid rule on a ellipse
+    delta = 2.0*pi/N
     for n = 0:N - 1
-        theta = n*DeltaTheta
+        theta = n * delta
         c = cos(theta)
         s = sin(theta)
-        
-        z1 = ellip.semi_x*c + ellip.semi_y*s
+		z1 = complex(ellip.semi_x * c, ellip.semi_y * s)
+		dz = complex(ellip.semi_y * c, ellip.semi_x * s) / N 
+		A0 = A0 + T * Vhat * dz
+		A1 = A1 + z1 * T * Vhat * dz
     end
 
     # SVD of A0
-    (V, Σ, W) = svd(A0)
+    (V, Sig, W) = svd(A0)
 
     tol = 1.0e-8
-    k = length(findall(abs(Σ) .> tol))
+    k = length(findall(abs(Sig) .> tol))
     println("Find $(k) nonzero singular values.")
 
     # perform a rank test
 
     # compute the matrix B
     Vk = V[:, 1:k]
-    Simgak = Σ[1:k]
+    Sigk = Sig[1:k]
     Wk = W[:, 1:k]
-    B = Vk' * A1 * Wk * diagm(1 ./ Sigmak)
+    B = Vk' * A1 * Wk * diagm(1 ./ Sigk)
 
     # solve the eigenvalue problem for B
-    λ = eigvals(B)
+    lambda = eigvals(B)
     V = eigvecs(B)
 
-    return λ, V
-    
+    return lambda, V
 end
