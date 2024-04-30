c0a58806c407a588c611027bb7769da4353f1758
peakfind <peakfind@126.com>
2024-04-30 20:19:50 +0800

    add show_eigs()

--- geometry.jl
+++ geometry.jl
@@ -4,7 +4,7 @@ using Plots
 # contours that we plan to support:
 #     1. ellipse
 #     2. circle
-#     3. rectangle (not finished)
+#     3. rectangle (not supported yet)
 abstract type AbstractContour end
 
 struct ellipse <: AbstractContour
@@ -25,8 +25,9 @@ struct circle <: AbstractContour
 end
 
 struct quadpts
-    N::Int64                      # the number of the quadrature nodes
-    nodes::AbstractArray{Float64} # quadrature nodes
+    N::Int64                            # the number of the quadrature nodes
+    nodes::AbstractArray{Float64}       # quadrature nodes size of N x 2 (we are in 2d now)
+    nodes_prime::AbstractArray{Float64} # derivative of the parametrization size of N x 2
 end
 
 function show_contr(contour::AbstractContour)
@@ -52,25 +53,34 @@ end
 # TODO: we may add the derivative of the parametrization in quadpts
 function get_quadpts(contour::AbstractContour, num_quadpts::Int64)
     nodes = zeros(num_quadpts, 2)
+    nodes_prime = zeros(num_quadpts, 2)
     delta = 2*pi / num_quadpts
 
     if typeof(contour) == ellipse
         for i = 0:num_quadpts-1
             nodes[i + 1, 1] = contour.center[1] + contour.semi_x * cos(delta * i)
             nodes[i + 1, 2] = contour.center[2] + contour.semi_y * sin(delta * i)
+            nodes_prime[i + 1, 1] = -contour.semi_x * sin(delta * i)
+            nodes_prime[i + 1, 2] = contour.semi_y * cos(delta * i)
         end
     elseif typeof(contour) == circle
-        for i = 0:N-1
+        for i = 0:num_quadpts-1
             nodes[i + 1, 1] = contour.center[1] + contour.radius * cos(delta * i) 
             nodes[i + 1, 2] = contour.center[1] + contour.radius * sin(delta * i)
+            nodes_prime[i + 1, 1] = -contour.radius * sin(delta * i)
+            nodes_prime[i + 1, 2] = contour.radius * cos(delta * i)
         end
     else
         throw(ArgumentError("unspported type of contour"))
     end
 
-    return quadpts(num_quadpts, nodes)
+    return quadpts(num_quadpts, nodes, nodes_prime)
 end
 
 function show_quadpts(pts::quadpts)
-    display(scatter!(pts.nodes[:,1], pts.nodes[:,2], markershape=:circle, markercolor=:blue))
+    display(scatter!(pts.nodes[:,1], pts.nodes[:,2], markershape=:circle, markercolor=:gray, markersize=2, aspect_ratio=:equal))
+end
+
+function show_eigs(eigs::AbstractArray)
+    display(scatter!(real(eigs), imag(eigs), markershape=:xcross, markercolor=:black, markersize=3, aspect_ratio=:equal))
 end
