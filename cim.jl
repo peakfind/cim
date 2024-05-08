39924ec3182407d321572c1cc451680913e5cac1
peakfind <peakfind@126.com>
2024-05-08 12:43:16 +0800

    new contr_int_ho()

--- cim.jl
+++ cim.jl
@@ -61,59 +61,10 @@ In this function, we follow the notations in Stefan Guttel, Francoise Tisseur, A
 r,l : size of the probing matrices
 """
 function contr_int_ho(pts::quadpts, NEP::Function, D::Int64, l::Int64, r::Int64, pbar::Int64)
-    # initialization
-    L = randn(ComplexF64, D, l)
-    R = randn(ComplexF64, D, r)
-    A = zeros(ComplexF64, l, r, 2*pbar)# contains all the moments p = 0, ..., 2*pbar - 1
-
-    # compute moments
-    # TODO: we need use NEP\R not the inv()
-    for j in 1:pts.N
-        z = complex(pts.nodes[j, 1], pts.nodes[j, 2])
-        z_prime = complex(pts.nodes_prime[j, 1], pts.nodes_prime[j, 2])
-        for p in 1:2*pbar
-            A[:,:,p] = A[:,:,p] + (L' * (NEP(z)\R)) * (z^p) * z_prime
-        end
-    end
-
-    A = A / (pts.N * im)
-
-    # compute B0 and B1
-    B0 = zeros(ComplexF64, l*pbar, r*pbar)
-    B1 = zeros(ComplexF64, l*pbar, r*pbar)
-
-    for j = 1:pbar
-        B0[1+(j-1)*l:j*l,:] = reshape(A[:,:,j:j+pbar-1], l,:)
-        B1[1+(j-1)*l:j*l,:] = reshape(A[:,:,j+1:j+pbar], l,:) 
-    end
-
-    # svd of B0
-    println("Compute the SVD of B0.")
-    (V, Sigma, W) = svd(B0)
-
-    # mbar: the number of the eigs inside the contour
-    tol = 1.0e-12
-    mbar = count(Sigma/Sigma[1] .> tol)
-    println("Find $(mbar) nonzero singular values.")
-
-    # compute the matrix M
-    Vmbar = V[:,1:mbar]
-    Sigmbar = Sigma[1:mbar]
-    Wmbar = W[:,1:mbar]
-
-    M = (Vmbar' * B1 * Wmbar) * Diagonal(1 ./ Sigmbar)
-
-    # compute the eigenvalues of M
-    lambda = eigvals(M)
-
-    return lambda
-end
-
-function contr_int_ho_gpt(pts::quadpts, NEP::Function, D::Int64, l::Int64, r::Int64, pbar::Int64)
     # Preallocate arrays
     L = randn(ComplexF64, D, l)
     R = randn(ComplexF64, D, r)
-    A = zeros(ComplexF64, l, r, 2*pbar)# contains all the moments p = 0, ..., 2*pbar - 1
+    A = zeros(ComplexF64, 2*pbar*l, r)# contains all the moments p = 0, ..., 2*pbar - 1
 
     # Compute moments (We need use NEP\R not the inv())
     for j in 1:pts.N
@@ -121,7 +72,7 @@ function contr_int_ho_gpt(pts::quadpts, NEP::Function, D::Int64, l::Int64, r::In
         z_prime = complex(pts.nodes_prime[j, 1], pts.nodes_prime[j, 2])
         L_invNEP_R = L' * (NEP(z) \ R)
         for p in 1:2*pbar
-            A[:,:,p] .+= L_invNEP_R * (z^p) * z_prime
+            A[(p-1)*l+1:p*l,:] .+= L_invNEP_R * (z^p) * z_prime
         end
     end
 
@@ -132,8 +83,8 @@ function contr_int_ho_gpt(pts::quadpts, NEP::Function, D::Int64, l::Int64, r::In
     B1 = zeros(ComplexF64, l*pbar, r*pbar)
 
     for j = 1:pbar
-        B0[1+(j-1)*l:j*l,:] = reshape(A[:,:,j:j+pbar-1], l,:)
-        B1[1+(j-1)*l:j*l,:] = reshape(A[:,:,j+1:j+pbar], l,:) 
+        B0[:,(j-1)*r+1:j*r] = @view A[(j - 1)*l+1:(pbar+j-1)*l,:]
+        B1[:,(j-1)*r+1:j*r] = @view A[j*l+1:(pbar+j)*l,:]
     end
 
     # Compute the SVD of B0
